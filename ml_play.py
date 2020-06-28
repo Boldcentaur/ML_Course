aa128706bf17fe0fae399c981a537b7bec243f97
Boldcentaur <38422196+Boldcentaur@users.noreply.github.com>
2020-06-29 02:28:21 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -94,10 +94,7 @@ class MLPlay:
             return move(grid= grid, coin_grid=coin_grid, speed_ahead = speed_ahead)
             
         def move(grid, coin_grid, speed_ahead):
-            if(2 not in grid and 2 in coin_grid):
-                self.commands = ["SPEED"]
-                return ["SPEED"]
-            elif(1 not in grid and 4 not in grid and 5 not in grid and (1 in coin_grid or 4 in coin_grid)):
+            if(1 not in grid and 4 not in grid and 5 not in grid and (1 in coin_grid or 4 in coin_grid)):
                 self.commands = ["SPEED", "MOVE_LEFT"]
                 return ["SPEED", "MOVE_LEFT"]
             elif(3 not in grid and 6 not in grid and 5 not in grid and (3 in coin_grid or 6 in coin_grid)):
